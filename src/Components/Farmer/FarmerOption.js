4f609a7209cf684a9b16f66076c70a2cdfb57b0c
Urmi Biswas <139246745+biswasurmi@users.noreply.github.com>
2024-08-25 13:11:03 +0600

    commit

--- src/Components/Farmer/FarmerOption.js
+++ src/Components/Farmer/FarmerOption.js
@@ -8,30 +8,30 @@ import './FarmerOption.css';
 
 export default function FarmerOption() {
     return (
-        <div className='bg-brand'>
+        <div className='farmer-option'>
             <div className='container py-5'>
                 <div className='d-flex flex-wrap flashsale-container'>
-                    <div className='col-lg-2 col-md-4 col-sm-6 p-2 card-body'>
+                    <div className='col-lg-3 col-md-4 col-sm-6 p-2 d-flex flex-column card-body'>
                         <img src={inspect} className='img-fluid' alt="Product Inspection" />
-                        <Link to="/addproductforinspection">
+                        <Link to="/addproductforinspection" className='mt-auto'>
                             <button className='btn btn-primary w-100 mt-2'>Add Product For Inspection</button>
                         </Link>
                     </div>
-                    <div className='col-lg-2 col-md-4 col-sm-6 p-2 card-body'>
+                    <div className='col-lg-3 col-md-4 col-sm-6 p-2 d-flex flex-column card-body'>
                         <img src={bid} className='img-fluid' alt="Auction" />
-                        <Link to="/auctionhistorypage">
+                        <Link to="/auctionhistorypage" className='mt-auto'>
                             <button className='btn btn-primary w-100 mt-2'>Add Product For Auction</button>
                         </Link>
                     </div>
-                    <div className='col-lg-2 col-md-4 col-sm-6 p-2 card-body'>
+                    <div className='col-lg-3 col-md-4 col-sm-6 p-2 d-flex flex-column card-body'>
                         <img src={sale} className='img-fluid' alt="Sale" />
-                        <Link to="/salehistorypage">
+                        <Link to="/salehistorypage" className='mt-auto'>
                             <button className='btn btn-primary w-100 mt-2'>Add Product For Sale</button>
                         </Link>
                     </div>
-                    <div className='col-lg-2 col-md-4 col-sm-6 p-2 card-body'>
+                    <div className='col-lg-3 col-md-4 col-sm-6 p-2 d-flex flex-column card-body'>
                         <img src={account} className='img-fluid' alt="Account Details" />
-                        <Link to="/accountdetails">
+                        <Link to="/accountdetails" className='mt-auto'>
                             <button className='btn btn-primary w-100 mt-2'>Account Details</button>
                         </Link>
                     </div>
