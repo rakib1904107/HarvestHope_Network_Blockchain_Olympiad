65d5d11c51c083ba393be73f30a373dceb8aa8c5
Urmi Biswas <139246745+biswasurmi@users.noreply.github.com>
2024-08-25 15:37:41 +0600

    commit

--- src/Components/Farmer/AuctionList.js
+++ src/Components/Farmer/AuctionList.js
@@ -3,10 +3,14 @@ import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './AuctionList.css';
 import d1 from '../Farmer/images/tomato.jpg';
 import d2 from '../Farmer/images/spinach.jpg';
-import d3 from '../Farmer/images/pineapple.jpg'
+import d3 from '../Farmer/images/pineapple.jpg';
 import d4 from '../Farmer/images/cucum.jpg';
 import d5 from '../Farmer/images/carrot.jpg';
 import d6 from '../Farmer/images/pepper.jpg';
+import d7 from '../Farmer/images/apple.jpg';
+import d8 from '../Farmer/images/potato.jpg';
+import d9 from '../Farmer/images/orange.jpg';
+import d10 from '../Farmer/images/broccoli.jpg';
 
 const AuctionList = () => {
     const items = [
@@ -16,6 +20,10 @@ const AuctionList = () => {
         { name: 'Cucumber', price: '200tk', imgSrc: d4, id: 4 },
         { name: 'Carrot', price: '350tk', imgSrc: d5, id: 5 },
         { name: 'Pepper', price: '450tk', imgSrc: d6, id: 6 },
+        { name: 'Apple', price: '600tk', imgSrc: d7, id: 7 },
+        { name: 'Potato', price: '150tk', imgSrc: d8, id: 8 },
+        { name: 'Orange', price: '550tk', imgSrc: d9, id: 9 },
+        { name: 'Broccoli', price: '400tk', imgSrc: d10, id: 10 },
     ];
 
     return (
