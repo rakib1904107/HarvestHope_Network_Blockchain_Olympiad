fff8f8b2f545f3363a3441e62ad031be56ed0ff3
Urmi Biswas <139246745+biswasurmi@users.noreply.github.com>
2024-08-25 16:27:38 +0600

    commit

--- src/Components/Farmer/AuctionCreation.js
+++ src/Components/Farmer/AuctionCreation.js
@@ -1,73 +1,83 @@
 import React from 'react';
 import './AuctionCreation.css';
-
+import {Link} from 'react-router-dom';
 const AuctionCreation = () => {
     return (
         <div className="add-inspect">
             <div className="add-inspect-container"></div>
-                <div className="form-page">
-                    <form>
-                        <div className="form-group">
-                            <label htmlFor="productName">Product Name</label>
-                            <input
-                                type="text"
-                                id="productName"
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="productImage">Product Image</label>
-                            <input
-                                type="file"
-                                id="productImage"
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="productDescription">Product Description</label>
-                            <textarea
-                                id="productDescription"
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="categories">Categories</label>
-                            <input
-                                type="text"
-                                id="categories"
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="quantity">Quantity</label>
-                            <input
-                                type="number"
-                                id="quantity"
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="quanlity">Quality</label>
-                            <input
-                                type="number"
-                                id="quality"
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="price">Price</label>
-                            <input
-                                type="number"
-                                id="price"
-                                required
-                            />
-                        </div>
+            <div className="form-page">
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="productName">Product Name</label>
+                        <input
+                            type="text"
+                            id="productName"
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="productImage">Product Image</label>
+                        <input
+                            type="file"
+                            id="productImage"
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="productDescription">Product Description</label>
+                        <textarea
+                            id="productDescription"
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="categories">Categories</label>
+                        <input
+                            type="text"
+                            id="categories"
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="quantity">Quantity</label>
+                        <input
+                            type="number"
+                            id="quantity"
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="quality">Quality</label>
+                        <input
+                            type="number"
+                            id="quality"
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="price">Price</label>
+                        <input
+                            type="number"
+                            id="price"
+                            required
+                        />
+                    </div>
+                    {/* New Time Field */}
+                    <div className="form-group">
+                        <label htmlFor="time">Time</label>
+                        <input
+                            type="time"
+                            id="time"
+                            required
+                        />
+                    </div>
+                    <Link to = "/ongoingauction">
                         <button type="submit" className="submit-button">Submit</button>
-                    </form>
-                </div>
+                    </Link>
+                    
+                </form>
             </div>
-       
-
+        </div>
     );
 };
 
